4c1abeaa52f8e1e2a125f901c115c155fd2eae95
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2023-05-05 14:36:53 +0500

    feat: solve 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+//Модуль path предназначен для того, чтобы работать с путями в Node.js. При помощи него можно получить имя файла, расширение файла, имя папки, указать путь к файлу.
+//Модуль fs (file system) нужен для работы с файлами и папками. Он умеет создавать и удалять файлы и папки, переименовывать их, записывать и считывать данные.
+//__dirname - строка, которая содержитт путь к текущей директории. Можем получить абсолютный путь на любои устройстве
+const fs = require('fs');
+const path = require('path');
+//console.log(path.join(__dirname, 'text.txt'), 'utf-8') //C:\Users\kir38\OneDrive\Desktop\rs_school\HTML-builder\01-read-file\text.txt
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+let result = '';
+stream.on('data', chunk => result += chunk);
+stream.on('end', () => console.log(`${result}`));
+stream.on('error', error => console.log('Error', error.message));
